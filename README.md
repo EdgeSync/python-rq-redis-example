4bc4ee6083b27d0bac2722da33315103e28a9a7b
stauntoc <chris.staunton@hpe.com>
2018-10-17 09:13:36 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
 # python-rq-redis-example
+
+I recently learned how to use redis to distrubte functions of a python program, and allow the program to run multiple functons in parallel. What irked me the most was how little documentation or straight-forward examples were out there to help me get up and runnning.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+python 2.7
+screen
+redis-server
+pip
+python modules rq and redis
+
+
+#### Installing necessary pre-reqs
+```
+sudo *package manager* install *software*
+e.g.
+
+sudo apt install screen
+sudo apt install redis-server
+
+pip installer downloaded from here: https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
+
+```
+
+#### Installing required python packages
+
+
+```
+e.g. via pip:
+
+pip install rq
+pip install redis
+```
+
+
+### Start the redis server
+
+```
+in a terminal, type:
+redis-server
+```
+
+### Run main_program.py
+```
+python main_program.py
+```
+
+### What happens next
+The screen starts, we can access it via a terminal like so:
+```
+screen -r sample_screen
+```
+The screen runs a worker monitoring on sample_queue
+
+Then the job is passed to the queue sample_queue, and the worker executes function_a() 
+
+
+In our screen (sample_screen) we can see function a being run - it counts 1-20.
+
+  
+#### Further Reading
+###### About Redis
+Redis contains 3 seperate components  
+  
+- The main component is the worker - this is seperate from the redis web server and is used to run the job  
+- The next component is the message queue - the worker constantly checks the message queue for new jobs
+- When a job appears in a message queue the worker assigns itself to the job, and run the job - then post results back to the queue 
+the last component is the task queue client - this component runs as part of the application in redis-server.  
+- This component posts new jobs to the message queue 
+- Documentation is available here: https://redis.io/
+
---
+++ main_program.py
@@ -0,0 +1,56 @@
+# Sample Main Program
+# Sets up the queues and workers and passes functions to them.
+#
+#
+# Copyright Chris Staunton
+# https://github.com/EdgeSync/python-rq-redis-example
+
+import os
+import rq
+from redis import Redis
+import time
+
+def create_worker():
+    pass
+    # First we create a worker, and tell it what job queue it needs to listen to.
+    # I like to use screen for my workers - however if you want to just run in a normal terminal window
+    # comment out create_worker() in the main function and then type the following:
+    # rq worker sample_queue
+
+    #Kills all previous running screens
+    os.system('pkill screen')
+    #Creates a new screen called "sample screen" and executes the command "rq worker sample_queue"
+    os.system('/usr/bin/screen -S sample_screen -d -m /usr/local/bin/rq worker sample_queue')
+
+def worker_a():
+    a_queue = rq.Queue('sample_queue', connection=Redis.from_url('redis://'))
+    a_job = a_queue.enqueue('main_program.function_a', timeout=60)  # We create a job variable.
+                                                                    # We tell it to push a_job job into a_queue
+                                                                    # We then pass it the function which is in main_program.py, function_a().
+                                                                    # As a sample i have put in a Timeout - this will kill the job if it takes more than 30 seconds.
+def function_a():
+    count = 1
+    while count <= 20:
+        print count
+        count += 1
+        time.sleep(1)
+
+def main():
+    # Used to run the above functions
+    print "Creating worker..."
+    create_worker()
+    time.sleep(5) # not required - just for this example. gives us time to attach to the screen before the worker starts.
+    print "Starting function a"
+    worker_a()
+
+if __name__ == '__main__':
+    main()
+
+# Redis contains 3 seperate components
+# the main component is the worker - this is seperate from the redis web server and is used to run the job
+# the next component is the message queue - the worker constantly checks the message queue for new jobs.
+# when a job appears in a message queue the worker assigns itself to the job, and run the job - then post results back to the queue
+# the last component is the task queue client - this component runs as part of the application in redis-server.
+# This component posts new jobs to the message queue
+
+
